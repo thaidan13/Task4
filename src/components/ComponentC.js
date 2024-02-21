513e66a0fcecffc8de7285d8ee9a69a0637585e1
YinGH <103986347+thaidan13@users.noreply.github.com>
2024-02-21 11:01:26 +0700

    ok

--- src/components/ComponentC.js
+++ src/components/ComponentC.js
@@ -3,8 +3,16 @@ import React from 'react';
 const ComponentC = ({ selectedLink }) => {
   return (
     <div>
-      <h2>Selected Link:</h2>
-      <iframe title="Selected Link" src={selectedLink} width="800" height="600"></iframe>
+      <h2>Selected Video:</h2>
+      <iframe 
+        title="Selected Video" 
+        width="800" 
+        height="600" 
+        src={selectedLink} 
+        frameborder="0" 
+        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" 
+        allowfullscreen
+      ></iframe>
     </div>
   );
 };
